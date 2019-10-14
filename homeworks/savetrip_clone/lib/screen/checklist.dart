02b6881fe0f6cd3e220a7702d5491d2cf36463a9
Hyeonwook Kim <hyeonwook.kim@navercorp.com>
2019-10-14 10:03:02 +0900

    Add FutureBuilder

--- homeworks/savetrip_clone/lib/screen/checklist.dart
+++ homeworks/savetrip_clone/lib/screen/checklist.dart
@@ -1,43 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_app_sample/main.dart';
+import 'dart:convert';
 
-class ChecklistView extends StatefulWidget {
+
+class ChecklistView extends StatelessWidget {
   ChecklistView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<ChecklistModel>>(
+      future: fetchChecklists(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ChecklistViewStf(key, snapshot.data);
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+
+        // By default, show a loading spinner.
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
+
+
+class ChecklistViewStf extends StatefulWidget {
+  List<ChecklistModel> _items;
+
+  ChecklistViewStf(Key key, List<ChecklistModel> items) : super(key: key) {
+    this._items = items;
+  }
 
   @override
   ChecklistViewState createState() => ChecklistViewState();
 }
 
-class ChecklistViewState extends State<ChecklistView> {
-  List<ChecklistModel> _items;
+class ChecklistViewState extends State<ChecklistViewStf> {
   int _selectedTag = 0;
 
   final itemTextController = TextEditingController();
 
-  @override
-  void initState() {
-    _items = [
-      new ChecklistModel(content: "담요 2장"),
-      new ChecklistModel(content: "속옷 5벌"),
-      new ChecklistModel(content: "양말 6개"),
-      new ChecklistModel(content: "바디로션"),
-      new ChecklistModel(content: "샴푸"),
-      new ChecklistModel(content: "국제운전면허증"),
-      new ChecklistModel(content: "여행용충전기"),
-      new ChecklistModel(content: "물티슈"),
-      new ChecklistModel(content: "카메라", isChecked: true),
-      new ChecklistModel(content: "여권", isChecked: true),
-      new ChecklistModel(content: "선글라스", isChecked: true),
-      new ChecklistModel(content: "아이패드", isChecked: true)
-    ];
-  }
-
   void addItem() {
     if (itemTextController.text.isNotEmpty) {
       setState(() {
-        _items.insert(0, ChecklistModel(content: itemTextController.text, tag: _selectedTag));
+        widget._items.insert(0, ChecklistModel(content: itemTextController.text, tag: _selectedTag));
         itemTextController.clear();
       });
     }
@@ -58,9 +68,9 @@ class ChecklistViewState extends State<ChecklistView> {
               onPressed: () {
                 setState(() {
                   if(_selectedTag == 0) {
-                    _items = [];
+                    widget._items = [];
                   } else {
-                    _items.removeWhere((i) => i.tag == _selectedTag);
+                    widget._items.removeWhere((i) => i.tag == _selectedTag);
                   }
                   Navigator.of(context).pop();
                 });
@@ -81,7 +91,7 @@ class ChecklistViewState extends State<ChecklistView> {
   @override
   Widget build(BuildContext context) {
     final floatingButtonBloc = Provider.of<FloatingButtonBloc>(context); // Counter 타입의 데이터를 가져옴.
-    List<ChecklistModel> itemsToShow = _selectedTag == 0 ? _items : _items.where((i) => i.tag == _selectedTag).toList();
+    List<ChecklistModel> itemsToShow = _selectedTag == 0 ? widget._items : widget._items.where((i) => i.tag == _selectedTag).toList();
     return Container(
       decoration: BoxDecoration(
         color: Colors.grey,
@@ -232,15 +242,15 @@ class ChecklistViewState extends State<ChecklistView> {
                       onChecked: (param) {
                         setState(() {
                           var selectedId = itemsToShow[param['index']].id;
-                          var matchedIndex = _items.indexWhere((i) => i.id == selectedId);
-                          _items[matchedIndex].isChecked = param['value'];
+                          var matchedIndex = widget._items.indexWhere((i) => i.id == selectedId);
+                          widget._items[matchedIndex].isChecked = param['value'];
                         });
                       },
                       onEdited: (index) {
                         setState(() {
                           var selectedId = itemsToShow[index].id;
-                          var matchedIndex = _items.indexWhere((i) => i.id == selectedId);
-                          ChecklistModel item = _items.removeAt(matchedIndex);
+                          var matchedIndex = widget._items.indexWhere((i) => i.id == selectedId);
+                          ChecklistModel item = widget._items.removeAt(matchedIndex);
                           itemTextController.text = item.content;
                           _selectedTag = item.tag;
                         });
@@ -248,8 +258,8 @@ class ChecklistViewState extends State<ChecklistView> {
                       onRemoved: (index) {
                         setState(() {
                           var selectedId = itemsToShow[index].id;
-                          var matchedIndex = _items.indexWhere((i) => i.id == selectedId);
-                          _items.removeAt(matchedIndex);
+                          var matchedIndex = widget._items.indexWhere((i) => i.id == selectedId);
+                          widget._items.removeAt(matchedIndex);
                         });
                       },
                     ),
@@ -333,10 +343,38 @@ class ChecklistModel {
   String content;
   bool isChecked;
   int tag;
-  Uuid id;
+  String id;
 
-  ChecklistModel({this.content, this.isChecked=false, this.tag = 1}) {
-    id = new Uuid();
+  ChecklistModel({this.content, this.isChecked=false, this.tag=1, this.id}) {
+    if (this.id == null) {
+      this.id = (new Uuid()).toString();
+    }
+  }
+
+  factory ChecklistModel.fromJson(Map<String, dynamic> json) {
+    return ChecklistModel(
+      content: json['content'],
+      isChecked: json['isChecked'],
+      tag: json['tag'],
+      id: json['id'],
+    );
+  }
+}
+
+Future<List<ChecklistModel>> fetchChecklists() async {
+  final response = await http.get('https://flutter-flexer.firebaseapp.com/checklists');
+
+  if (response.statusCode == 200) {
+    // If server returns an OK response, parse the JSON.
+    List<dynamic> jsonList = json.decode(response.body);
+    List<ChecklistModel> list = [];
+    for (final i in jsonList) {
+      list.add(ChecklistModel.fromJson(i));
+    }
+    return list;
+  } else {
+    // If that response was not OK, throw an error.
+    throw Exception('Failed to load checklist');
   }
 }
 
